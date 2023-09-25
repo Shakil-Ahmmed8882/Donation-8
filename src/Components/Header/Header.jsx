75dfaf10462808428b2252bad477b5724689bfba
Shakil-Ahmmed <Shakilahmmed8882@gmail.com>
2023-09-25 21:52:15 +0600

    Designed banner section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,43 @@
-import Navbar from "./Navbar";
-
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const Header = () => {
-      return (
-            <div className="border-b-2 border border-[#b6b6b63b]">
-                  <Navbar></Navbar>
-            </div>
+  const location = useLocation();
+  const currentPath = location.pathname;
 
+  const isHomePage = currentPath === '/home';
 
-      );
+  // State to track is being scrolled or not
+  const [headerRedBackground, setHeaderRedBackground] = useState(false);
+
+  // handling the scroll event
+  const handleScroll = () => {
+    if (window.scrollY >= 420) {
+      setHeaderRedBackground(true);
+    } else {
+      setHeaderRedBackground(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+      
+    // removing the lister when no longer in need
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`border-b-2 ${isHomePage && 'fixed'} w-full top-0 border border-[#b6b6b63b] ${
+        headerRedBackground ? 'bg-white' : 'bg-[#ffffff73]'
+      }`}
+    >
+      <Navbar />
+    </div>
+  );
 };
 
 export default Header;
--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -1,9 +1,10 @@
-
-
+import './home.css'
+import PropTypes from 'prop-types'
 const Banner = ({setMatchedWord,setInputValue}) => {
       const matchedElements = ['education','health','clothing','food']
 
       // handle search
+      
       const handleSearch = (e) =>{
              setInputValue(e.target.value)
             const value =  e.target.value.toLowerCase()
@@ -19,17 +20,22 @@ const Banner = ({setMatchedWord,setInputValue}) => {
 
 
       return (
-            <div className="w-full h-[60vh] bg-[#f4f3ff] flex flex-col items-center justify-center">
-                        <h1 className="text-5xl text-black font-bold py-3">I Grow By Helping People In Need</h1>
-                        <label className="flex mt-8">
-                              <div className="py-2 px-3 border-[1px] w-80 border-r-none border-[#9e9e9e78] bg-white">
+            <div className="w-full h-[75vh] banner-bg flex flex-col items-center justify-center">
+                        <h1 className="text-3xl text-center md:text-5xl mt-20 text-black font-bold py-3">I Grow By Helping People In Need</h1>
+                        <label className="flex mt-8 w-full px-4 justify-center text-[10px] md:text-[16px]">
+                              <div className="py-2  border-[1px] w-[80%] sm:w-[50%] md:w-[40%] px-3 border-r-none border-[#9e9e9e78] bg-white">
                               <input onChange={handleSearch} className="bg-transparent outline-none" type="search" name="" id="" placeholder="Search here.." />
                               </div>
-                              <button className="bg-[#FF444A] text-white p-3 rounded-r">Search</button>
-
+                              <button className="bg-[#FF444A] text-white md:p-3 md:px-6 rounded-r">Search</button>
                         </label>
             </div>
       );
 };
+
+
+Banner.propTypes = {
+      setMatchedWord:PropTypes.func.isRequired,
+      setInputValue : PropTypes.func.isRequired
+}
 
 export default Banner;
